e0472fd78e5be1fa7767a6fd4a1678852aecd73c
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2021-09-30 16:09:24 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,41 @@
 basic.forever(function () {
+    for (let _1 = 0; _1 <= 4; _1++) {
+        for (let index = 0; index <= 4; index++) {
+            led.toggle(4 - index, 4 - _1)
+            basic.pause(100)
+            led.toggle(4 - index, 4 - _1)
+        }
+    }
     for (let index = 0; index <= 4; index++) {
         for (let _1 = 0; _1 <= 4; _1++) {
-            led.plot(index, _1)
+            led.toggle(index, _1)
+            basic.pause(100)
+            led.toggle(index, _1)
+        }
+    }
+    for (let _1 = 0; _1 <= 4; _1++) {
+        for (let index = 0; index <= 4; index++) {
+            if (_1 % 2 == 0) {
+                led.toggle(index, _1)
+            } else {
+                led.toggle(4 - index, _1)
+            }
+            basic.pause(100)
+        }
+    }
+    for (let _1 = 0; _1 <= 4; _1++) {
+        for (let index = 0; index <= 4; index++) {
+            if (_1 % 2 == 0) {
+                led.toggle(4 - index, 4 - _1)
+            } else {
+                led.toggle(index, 4 - _1)
+            }
+            basic.pause(100)
+        }
+    }
+    for (let index = 0; index <= 4; index++) {
+        for (let _1 = 0; _1 <= 4; _1++) {
+            led.toggle(index, _1)
             basic.pause(100)
         }
     }
