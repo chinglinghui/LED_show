bc3414986a295db905888f5c091942cd04dc7b6a
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2021-09-30 16:47:41 +0800

    試圖恢復原本的樣子

--- main.ts
+++ main.ts
@@ -1,47 +1,25 @@
 basic.forever(function () {
-    for (let _1 = 0; _1 <= 4; _1++) {
-        for (let index = 0; index <= 4; index++) {
-            led.toggle(4 - index, 4 - _1)
+    for (let X = 0; X <= 4; X++) {
+        for (let Y = 0; Y <= 4; Y++) {
+            led.toggle(4 - X, 4 - Y)
             basic.pause(100)
-            led.toggle(4 - index, 4 - _1)
+            led.toggle(4 - X, 4 - Y)
         }
     }
-    for (let index = 0; index <= 4; index++) {
-        for (let _1 = 0; _1 <= 4; _1++) {
-            led.toggle(index, _1)
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.toggle(X, Y)
             basic.pause(100)
-            led.toggle(index, _1)
+            led.toggle(X, Y)
         }
     }
-    for (let _1 = 0; _1 <= 4; _1++) {
-        for (let index = 0; index <= 4; index++) {
-            if (_1 % 2 == 0) {
-                led.toggle(index, _1)
+    for (let X = 0; X <= 4; X++) {
+        for (let Y = 0; Y <= 4; Y++) {
+            if (X % 2 == 0) {
+                led.toggle(X, Y)
             } else {
-                led.toggle(4 - index, _1)
+                led.toggle(X, 4 - Y)
             }
-            basic.pause(100)
-        }
-    }
-    for (let _1 = 0; _1 <= 4; _1++) {
-        for (let index = 0; index <= 4; index++) {
-            if (_1 % 2 == 0) {
-                led.toggle(4 - index, 4 - _1)
-            } else {
-                led.toggle(index, 4 - _1)
-            }
-            basic.pause(100)
-        }
-    }
-    for (let index = 0; index <= 4; index++) {
-        for (let _1 = 0; _1 <= 4; _1++) {
-            led.toggle(index, _1)
-            basic.pause(100)
-        }
-    }
-    for (let _1 = 0; _1 <= 4; _1++) {
-        for (let index = 0; index <= 4; index++) {
-            led.toggle(4 - index, 4 - _1)
             basic.pause(100)
         }
     }
