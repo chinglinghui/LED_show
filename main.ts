473cc0be458b1a47f9327505c72552fd1b6f6c24
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2021-10-07 16:37:29 +0800

    多一個輪流消失的步驟

--- main.ts
+++ main.ts
@@ -45,4 +45,10 @@ basic.forever(function () {
             basic.pause(100)
         }
     }
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.toggle(4 - X, 4 - Y)
+        }
+    }
+    basic.pause(100)
 })
