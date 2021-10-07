16e687935a952cc2ebb2bbb73c3b622dcff5e9fb
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2021-10-07 16:33:09 +0800

    將程式補回原來的樣子

--- main.ts
+++ main.ts
@@ -1,25 +1,47 @@
 basic.forever(function () {
-    for (let X = 0; X <= 4; X++) {
-        for (let Y = 0; Y <= 4; Y++) {
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
             led.toggle(4 - X, 4 - Y)
             basic.pause(100)
             led.toggle(4 - X, 4 - Y)
+        }
+    }
+    for (let X = 0; X <= 4; X++) {
+        for (let Y = 0; Y <= 4; Y++) {
+            led.toggle(X, Y)
+            basic.pause(100)
+            led.toggle(X, Y)
         }
     }
     for (let Y = 0; Y <= 4; Y++) {
         for (let X = 0; X <= 4; X++) {
-            led.toggle(X, Y)
+            if (Y % 2 == 0) {
+                led.toggle(X, Y)
+            } else {
+                led.toggle(4 - X, Y)
+            }
             basic.pause(100)
-            led.toggle(X, Y)
+        }
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            if (Y % 2 == 0) {
+                led.toggle(4 - X, 4 - Y)
+            } else {
+                led.toggle(X, 4 - Y)
+            }
+            basic.pause(100)
         }
     }
     for (let X = 0; X <= 4; X++) {
         for (let Y = 0; Y <= 4; Y++) {
-            if (X % 2 == 0) {
-                led.toggle(X, Y)
-            } else {
-                led.toggle(X, 4 - Y)
-            }
+            led.toggle(X, Y)
+            basic.pause(100)
+        }
+    }
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.toggle(4 - X, 4 - Y)
             basic.pause(100)
         }
     }
